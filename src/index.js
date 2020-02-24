7599ced6fbadc7118724346d1e6e86c435659eb4
dmitry1992.coding@gmail.com <dmitry1992.coding@gmail.com>
2020-02-24 15:40:45 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  let minElement = 0;
+  for (let i=0; i <= array.length; i++) {
+    if (array[i] < minElement) {
+      minElement = array[i];
+    }
+  }
+  return minElement;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  let maxElement = 0;
+  for (let i=0; i <= array.length; i++) {
+    if (array[i] > maxElement) {
+      maxElement = array[i];
+    }
+  }
+  return maxElement;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  let num =  array.reduce(function(total, elem) {
+    return (total + elem);
+  });
+  return num / array.length;
 }
